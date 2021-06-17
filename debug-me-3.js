754da53d2f0ea93c317ab31e887fd7b4c28f7cb0
Savannah Simonis <savannah.simonis@x-center.com>
2021-06-17 16:08:32 +0200

    Homework done

--- debug-me-3.js
+++ debug-me-3.js
@@ -1,10 +1,10 @@
 const weight = 110;
-const BMI = weight \ (height * height);
 const height = 1.79;
+const BMI = weight / (height * height);
 
 console.log("Your BMI is: " + BMI.toFixed());
 
-if (BMI < 25) {
+if (BMI > 25) {
   console.log("Depending on your build, you might be overweight");
 } else if (BMI < 18) {
   console.log("Depending on your build, you might be underweight");
